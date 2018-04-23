830a3ac11b0bc05009fe54dd1b02143d471142ff
blAs1N <hwisin210@naver.com>
2018-04-23 20:29:45 +0900

    Hello, C#!

---
+++ Hello, world.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cs_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+        }
+    }
+}
